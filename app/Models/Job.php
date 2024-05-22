d2f7205d31284463207f79f8f6848fbf5accebaf
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-05-22 14:48:15 +0530

    Day 9 - Job Model extends Eloquent Model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,34 +1,10 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job{
-    public static function all(): array{
-        return [
-            [
-                'id' => 1,
-                'title' => 'Teacher',
-                'salary' => '$20,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Researcher',
-                'salary' => '$10,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Software Engineer',
-                'salary' => '$50,000',
-            ]
-        ];
-    }
+class Job extends Model {
+    protected $table = 'job_listing';
 
-    public static function find(int $id): array{
-       $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-       if(! $job){
-           abort(404, 'Job not found');
-       }
-       return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }
