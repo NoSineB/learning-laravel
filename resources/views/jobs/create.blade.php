6e4247a47a68a64f1230f4e07adbc1a5c89adfd0
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-05-25 20:07:05 +0530

    Day 16 - Validation added to route 'jobs/create'

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -14,19 +14,26 @@
                         <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Job Title</label>
                         <div class="mt-2">
                             <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Eg. Graphics Designer">
+                                <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Eg. Graphics Designer" required>
                             </div>
+                            @error('title')
+                                <p class="text-red-500 text-sm mt-1 italic">{{ $message  }}</p>
+                            @enderror
                         </div>
                     </div>
                 </div>
+
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <div class="sm:col-span-4">
                         <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
                         <div class="mt-2">
                             <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="$50,000 USD">
+                                <input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Eg. $50,000 USD" required>
                             </div>
+                            @error('salary')
+                            <p class="text-red-500 text-sm mt-1 italic">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
                 </div>
