fa20ff84ea524d8605140656c2b136bd17d00d59
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-05-25 19:28:12 +0530

    Day 16 - Forms for Job Listing Added

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,30 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->paginate(6);
-    return view("jobs", [
+    $jobs = Job::with('employer')->latest()->paginate(6);
+    return view("jobs/index", [
         'jobs' => $jobs
     ]);
 });
 
+Route::get('/jobs/create', function () {
+    return view("jobs/create");
+});
+
 Route::get('/jobs/{id}', function ($id) {
-    return view("job", [
+    return view("jobs/show", [
         'job' => Job::find($id)
     ]);
+});
+
+Route::post('/jobs', function () {
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => '2',
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
