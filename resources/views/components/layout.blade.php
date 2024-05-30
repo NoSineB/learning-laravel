6db87bae993d4fd9098c76d3154bf5932ae4bf80
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-05-30 12:49:19 +0530

    Day 25 - Vite Added

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,11 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Example</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        <script src="https://cdn.tailwindcss.com"></script>
+        @vite('resources/css/app.css')
     </head>
     <body class="h-full">
         <div class="min-h-full">
